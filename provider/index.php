e097058ff4b66680f90a5eae775c7c2056eff4f3
Angelo <cupertinoangelo13@gmail.com>
2022-07-09 17:38:06 -0300

    :sparkles: feat: Adicionar rota

---
+++ provider/index.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . "/vendor/autoload.php";
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+$app->addErrorMiddleware(false, true, false);
+
+$app->get("/person/{id}", function (Request $request, Response $response, array $args) {
+    $body = [
+        "first_name" => "Dade",
+        "last_name" => "Murphy",
+        "alias" => "Zero Cool",
+        "age" => 26,
+    ];
+    $response = $response->withHeader("Content-Type", "application/json");
+    $response->getBody()->write(json_encode($body));
+    return $response;
+});
+
+$app->run();

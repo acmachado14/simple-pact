05449c3115bb6cb4517041b8086c4dac21dac97e
Angelo <cupertinoangelo13@gmail.com>
2022-07-09 17:32:14 -0300

    :test_tube: test: Adicionar teste de contrato

---
+++ provider/tests/ConsumerTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Acmachado14\PactProvider;
+
+use GuzzleHttp\Psr7\Uri;
+use PhpPact\Standalone\ProviderVerifier\Model\VerifierConfig;
+use PhpPact\Standalone\ProviderVerifier\Verifier;
+use PHPUnit\Framework\TestCase;
+
+class ConsumerTest extends TestCase
+{
+    public function testPersonConsumer()
+    {
+        $config = new VerifierConfig();
+        $config->setProviderName("personProvider")
+            ->setProviderVersion("1.0.0")
+            ->setProviderBaseUrl(new Uri("http://localhost:8080"))
+            ->setBrokerUri(new Uri("http://localhost:9292"))
+            ->setPublishResults(true);
+
+        $verifier = new Verifier($config);
+        $verifier->verifyAll();
+
+        $this->assertTrue(true);
+    }
+
+}

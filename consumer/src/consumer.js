2a2863eb903efca0ec42dd76c7cdddf38b6dc142
Angelo <cupertinoangelo13@gmail.com>
2022-07-10 14:46:20 -0300

    :recycle: refactor: Alterar rota chamada

--- consumer/src/consumer.js
+++ consumer/src/consumer.js
@@ -2,8 +2,8 @@ const axios = require('axios')
 
 const baseUrl = `http://localhost:8001`;
 
-const getPerson = async () => {
-  return await axios.get(`${baseUrl}/api/v1/user/bankAccounts`);
+const getPerson = async (id) => {
+  return await axios.get(`${baseUrl}/person/${id}`);
 }
 
 module.exports = {
